f135ce147bde8eaf09327051aa69736e0d3c8fca
Shrishti Singhal <shrishtisinghal96@gmail.com>
2026-07-05 17:10:32 +0530

    updated

---
+++ dayone.py
@@ -0,0 +1,27 @@
+#variables
+a="apple"
+print(a)
+
+
+#basic operators
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+#identify datatype
+name="bbd"
+print(type(name))
+
+
+#to get user input
+a=input("enter name ")
+print("name is",a)
+print(type(a)) #by default user input is string
+
+b=int(input("enter no "))
+print(type(b))  #makes user input integer
